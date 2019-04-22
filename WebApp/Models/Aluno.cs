f01322903adf6b9359b6a21775d83d595481ad20
AdalbertoJR2 <adalbertopsjunior01@gmail.com>
2019-04-22 13:25:01 -0300

    Projeto Iniciado

---
+++ WebApp/Models/Aluno.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebApp.Models
+{
+    public class Aluno
+    {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string telefone { get; set; }
+        public int ra { get; set; }
+
+        public List<Aluno> ListarAlunos()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var listaAlunos = JsonConvert.DeserializeObject<List<Aluno>>(json);
+
+            return listaAlunos;
+        }
+
+        public bool ReescreverArquivos(List<Aluno> listaAlunos)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = JsonConvert.SerializeObject(listaAlunos, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+        public Aluno Inserir(Aluno Aluno)
+        {
+            var listaAlunos = this.ListarAlunos();
+
+            var maxId = listaAlunos.Max(aluno => aluno.id);
+            Aluno.id = maxId + 1;
+            listaAlunos.Add(Aluno);
+
+            ReescreverArquivos(listaAlunos);
+            return Aluno;
+        }
+
+        public Aluno Atualizar(int id, Aluno Aluno)
+        {
+            var listaAlunos = this.ListarAlunos();
+
+            var itemIndex = listaAlunos.FindIndex(p => p.id == id);
+            if(itemIndex >= 0)
+            {
+                Aluno.id = id;
+                listaAlunos[itemIndex] = Aluno;
+            }
+            else
+            {
+                return null;
+            }
+            ReescreverArquivos(listaAlunos);
+            return Aluno;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaAlunos = this.ListarAlunos();
+
+            var itemIndex = listaAlunos.FindIndex(p => p.id == id);
+            if(itemIndex >= 0)
+            {
+                listaAlunos.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+            ReescreverArquivos(listaAlunos);
+            return true;
+        }
+    }
+}
